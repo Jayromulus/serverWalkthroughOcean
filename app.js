dc7096864c4d238a2b33823b56087d8ebefa2ddb
Eli T. Drumm <eli@etd.is>
2020-10-12 16:14:41 -0800

    adding updates for 10/12

--- app.js
+++ app.js
@@ -14,21 +14,26 @@ database.sync();
 
 app.use(Express.json());
 
+app.use(require('./middleware/headers'));
+
 // SERVING A STATIC FILE
-app.use(Express.static(__dirname + '/public'));
-console.log(__dirname);
+// app.use(Express.static(__dirname + '/public'));
+// console.log(__dirname);
 
-app.get('/', (request, response) => response.render('index'));
+// app.get('/', (request, response) => response.render('index'));
 
-// import the pie controller
-const pies = require('./controllers/piecontroller');
+// // import the pie controller
+// const pies = require('./controllers/piecontroller');
 
 // use the piecontroller routes if we are at localhost:8080/pies
-app.use('/pies', pies);
+// app.use('/pies', pies);
 // '/pies' is going to prepend the /pies route before ANY of the routes from this pies controller, so if a route in the pie controller is just '/', we will have to go to '/pies/'
 
 const pizza = require('./controllers/pizzacontroller');
 app.use('/pizza', pizza);
+
+const user = require('./controllers/usercontroller');
+app.use('/user', user);
 
 // this will start our server on port 8080, and run a console.log telling us that the server is running
 app.listen(process.env.PORT, function() { console.log(`app is listening on ${process.env.PORT}`) });
--- controllers/pizzacontroller.js
+++ controllers/pizzacontroller.js
@@ -40,5 +40,12 @@ router.put('/:id', (req, res) => {
     .catch(err => res.status(500).json({ error:err }))
 })
 
+router.delete('/:id', (req, res) => {
+  Pizza.destroy({ where: { id: req.params.id } })
+    .then(result => res.status(200).json(result))
+    .catch(err => res.status(500).json({ error: err }))
+})
+
+
 
 module.exports = router;
